3f63ee7a10e392bb23be6322b556209dbda40250
Abah Gideon <abahgideon111@gmail.com>
2025-04-25 15:26:37 +0100

    profile upload bug fix

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -74,20 +74,34 @@
                         authStore.user.email.split("@")[0]
                       }}
                     </span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="size-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    <!-- Profile Picture or Fallback Icon -->
+                    <div class="relative">
+                      <img
+                        v-if="authStore.user.photoURL"
+                        :src="authStore.user.photoURL"
+                        alt="Profile"
+                        class="w-8 h-8 rounded-full object-cover border-2 border-green-100"
                       />
-                    </svg>
+                      <div
+                        v-else
+                        class="w-8 h-8 rounded-full bg-gray-200 flex items-center justify-center border-2 border-green-100"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-5 h-5 text-gray-500"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
                   </button>
                   <div
                     class="absolute right-0 w-48 bg-white rounded-md shadow-lg py-1 hidden group-hover:block"
@@ -138,20 +152,33 @@
             @click="toggleMobileMenu"
             class="md:hidden text-gray-500 hover:text-green-600 focus:outline-none"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-8"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            <div class="relative">
+              <img
+                v-if="authStore.isAuthenticated && authStore.user?.photoURL"
+                :src="authStore.user.photoURL"
+                alt="Profile"
+                class="w-8 h-8 rounded-full object-cover border-2 border-green-100"
               />
-            </svg>
+              <div
+                v-else
+                class="w-8 h-8 rounded-full bg-gray-200 flex items-center justify-center"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6 text-gray-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
+              </div>
+            </div>
           </button>
         </div>
       </div>
